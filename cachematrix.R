6d42da7c6c0fa4fb5eba4353f5859b0d855e77f0
tgjuranec <tgjuranec@gmail.com>
2015-04-14 19:43:17 +0200

    release candidat -> everything works

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Function for storing matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-	invert <- function(m){
-	}
+    minv <- NULL
+    set <- function(y){
+        m <<- y
+        minv <<- NULL
+    }
+    get <- function() m
+    setinverse <- function(minv_inp) minv <<- minv_inp
+    getinverse <- function() minv
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## Function for access makeCacheMatrix function and
+## solving or getting inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    minv <- m$getinverse()
+    if(!is.null(minv)) {
+        message("getting cached data")
+        return(minv)
+    }
+    data <- m$get()
+    inverse <- solve(data, ...)
+    m$setinverse(inverse)
+    inverse    
 }
